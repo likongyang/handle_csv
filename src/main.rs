3dd6be7c5cdcfcbe8c64c9650dcafa5bd1c32565
likongyang <likongyang@sightp.com>
2020-11-13 15:45:28 +0800

    更新并集算法, 复杂度为O(1)

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::collections::HashMap;
 /// 参数说明：
 /// file_path: 单个文件; total_file_path: 汇总文件； target_file_path: 要生成的文件路径；keyword_index：指定要进行对比的关键字所在的列数，从0开始;
 /// header：csv文件中是否含有标题行
-fn single_file_unrepeat(file_path: &PathBuf, total_file_path: &PathBuf, target_file_path: &PathBuf, keyword_index: usize, header: bool) -> i32 {
+fn file_complementary_subset(file_path: &PathBuf, total_file_path: &PathBuf, target_file_path: &PathBuf, keyword_index: usize, header: bool) -> i32 {
     // 生产目标输出文件的写指针、单个文件、汇总文件的指针
     let mut wtr = csv::WriterBuilder::new().has_headers(header).from_path(&target_file_path).unwrap();
     let mut rdr_total_file = csv::ReaderBuilder::new().has_headers(header).from_path(&total_file_path).unwrap();
@@ -59,8 +59,8 @@ fn single_file_unrepeat(file_path: &PathBuf, total_file_path: &PathBuf, target_f
     count
 }
 
-/// 用于多个文件之间交集
-/// 输出交集文件
+/// 用于多个文件之间并集
+/// 输出并集文件
 /// 参数说明：
 /// file_path:多个文件路径组成的vector, target_file_path：最终生成文件的路径；keyword_index：指定要进行对比的关键字所在的列数，从0开始;
 /// header：csv文件中是否含有标题行
@@ -80,52 +80,38 @@ fn multiple_file_cross_unrepeat(file_path: Vec<PathBuf>, target_file_path: &Path
     }
 
     // 核心算法：
-    // 以第一个文件作为基础，生成目标文件
-    // 从第二个文件开始，跟目标文件进行对比，如果对比没有该条记录，则把这一条记录写入目标文件当中
-    for first_file_record in rdr_first_file.records() {
-        match first_file_record {
-            Ok(first_file_record) => {
-                wtr.write_record(&first_file_record).unwrap();
-                count += 1;
-            },
-            Err(_) => eprintln!("write first file failed"),
-        }
-        
-    }
-    wtr.flush().unwrap();
-
-    // 部分与单个文件有类似的地方，就是比对的顺序不一致，以及由于单个文件的重新生成文件指针导致创建空白文件
-    // 尝试找出是否可以append或者只提炼比较的地方代码生成函数进行调用
-    for file_index in 1..file_path.len() {
+    // 以对每个文件的某一个列进行hash处理，指定列值为键，行作为值
+    // 利用hashmap更新的机制，把所有的文件的列和值都生成hashmap后再进行写入
+    let mut file_hashmap = HashMap::new();
+    for file_index in 0..file_path.len() {
         let mut rdr_file = csv::ReaderBuilder::new().has_headers(header).from_path(&file_path[file_index]).unwrap();
-
         for file_record in rdr_file.records() {
-            write_flag = true;
             match file_record {
                 Ok(file_record) => {
-                    let file_keyword: &str = &file_record[keyword_index];
-                    let mut rdr_target_file = csv::ReaderBuilder::new().has_headers(header).from_path(target_file_path).unwrap();
-                    for target_file_record in rdr_target_file.records() {
-                        match target_file_record {
-                            Ok(target_file_record) => {
-                                let target_file_keyword: &str = &target_file_record[keyword_index];
-                                if file_keyword == target_file_keyword {
-                                    write_flag = false;
-                                    break;
-                                }
-                            },
-                            Err(_) => eprintln!("could not read total file records"),
-                        }
-                    }
-                    if write_flag {
-                        wtr.write_record(&file_record).unwrap();
-                        count += 1;
+                    let file_keyword: String = file_record[keyword_index].to_owned();
+                    if file_hashmap.contains_key(&file_keyword) {
+                        continue;
+                    } else {
+                        file_hashmap.insert(file_keyword, file_record);
                     }
                 },
-                Err(_) =>eprintln!("could not get keyword"),
+                Err(_) => eprintln!("could not read file records"),
             }
         }
     }
+
+    // 写入到文件中
+    for key in file_hashmap.keys() {
+        let file_record = file_hashmap.get(key);
+        match file_record {
+            Some(file_record) => {
+                wtr.write_record(file_record);
+                count += 1;
+            },
+            _ => eprintln!("could not read file record"),
+        }
+    } 
+
     wtr.flush().unwrap();
 
     count
@@ -173,26 +159,28 @@ fn search_keyword(keyword: Vec<&str>, file_path: &PathBuf, target_file_path: &Pa
 
 fn main() {
     // // 测试文件路径
-    let file_one_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/chashuju.csv");
-    // // let file_two_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/chashuju_2.csv");
-    let total_file_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/company_data.csv");
+    let file_one_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/chashuju_1.csv");
+    let file_two_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/chashuju_2.csv");
+    // let total_file_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/company_data.csv");
     let target_file_path = PathBuf::from("/Users/likongyang/Desktop/test_wenlv/target_file.csv");
-    // // let file_path_vec = vec![file_one_path, file_two_path];
+    let file_path_vec = vec![file_one_path, file_two_path];
 
     // // 搜索关键字
     // let keyword = vec!["测试", "test"];
 
     // // 测试单个文件去重
     let start_time = std::time::Instant::now();
-    let count = single_file_unrepeat(&file_one_path, &total_file_path, &target_file_path, 0, true);
-    let end_time = std::time::Instant::now();
-    let cost_time = end_time.duration_since(start_time);
+    // let count = file_complementary_subset(&file_one_path, &total_file_path, &target_file_path, 0, true);
+    
 
     // // 测试多个文件之间的并集
-    // // let count = multiple_file_cross_unrepeat(file_path_vec, &target_file_path, 0, false);
+    let count = multiple_file_cross_unrepeat(file_path_vec, &target_file_path, 0, false);
 
     // // 测试搜索指定的文件
     // let count = search_keyword(keyword, &file_one_path, &target_file_path, false, 20);
+
+    let end_time = std::time::Instant::now();
+    let cost_time = end_time.duration_since(start_time);
     println!("耗时{:?}", cost_time);
 
     println!("一共有 {} 条数据生成", count);
